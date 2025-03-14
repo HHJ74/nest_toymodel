7df766779a6d51fae443ac6c0a556a1ba491cd2a
HHJ74 <gpgus0704@gmail.com>
2025-03-14 11:53:19 +0900

    link kakao signin signup

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, Get, Param } from '@nestjs/common';
 import { UsersService } from './user.service';
 
 
@@ -10,7 +10,21 @@ export class UsersController {
   @HttpCode(201)
   async signup(@Body() body: { kakao_id: number; nickname: string }) {
     const { kakao_id, nickname } = body;
-    const user = await this.usersService.createUser(kakao_id, nickname);
-    return { message: '회원가입 성공', user };
+    const user = await this.usersService.createOrGetUser(kakao_id, nickname); // 새 메서드 호출
+    return { message: '로그인 성공', user };
+  }
+  
+  // 새로 추가: 모든 사용자 조회
+  @Get('users')
+  async getAllUsers() {
+    const users = await this.usersService.getAllUsers();
+    return { message: '사용자 목록', users };
+  }
+
+  // 특정 kakao_id로 사용자 조회
+  @Get('user/:kakao_id')
+  async getUserByKakaoId(@Param('kakao_id') kakao_id: string) {
+    const user = await this.usersService.getUserByKakaoId(+kakao_id); // string -> number 변환
+    return { message: '사용자 조회 성공', user };
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -10,13 +10,28 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async createUser(kakao_id: number, nickname: string): Promise<User> {
-    const existingUser = await this.usersRepository.findOne({ where: { kakao_id } });
-    if (existingUser) {
-      throw new BadRequestException('이미 가입된 사용자입니다.');
+  // 회원가입 및 로그인
+  async createOrGetUser(kakao_id: number, nickname: string): Promise<User> {
+    let user = await this.usersRepository.findOne({ where: { kakao_id } });
+    if (!user) {
+      // 새 사용자 생성
+      user = this.usersRepository.create({ kakao_id, nickname });
+      user = await this.usersRepository.save(user);
     }
+    return user; // 기존 사용자면 바로 반환
+  }
 
-    const user = this.usersRepository.create({ kakao_id, nickname });
-    return this.usersRepository.save(user);
+  // 모든 사용자 조회
+  async getAllUsers(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  // kakao_id로 사용자 조회
+  async getUserByKakaoId(kakao_id: number): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { kakao_id } });
+    if (!user) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
+    }
+    return user;
   }
 }
